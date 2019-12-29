c27714de40f4224f04cb5761ad116c33a54499fa
ilyassmoutite <ilyassmoutite@gmail.com>
2019-12-29 14:59:12 +0100

    run

---
+++ destroy.bat
@@ -0,0 +1,1 @@
+vagrant destroy app1 app2 sentinel1 sentinel2 varnish2 dbr dbm glusterM glusterr proxy redisr redism
---
+++ run.bat
@@ -0,0 +1,1 @@
+vagrant up app1 app2 sentinel1 sentinel2 varnish2 dbr dbm glusterM glusterr proxy redisr redism
